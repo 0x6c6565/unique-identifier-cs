eb440a1b98c288fc113c5b5dc21d56ada80c9f8f
0x6c6565 <0x6c6565@gmail.com>
2026-09-21 17:17:36 -0400

    json converter

---
+++ runtime/Guid128JsonConverter.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+
+using UnityEngine;
+
+namespace UniqueIdentifiers
+{
+	public class Guid128JsonConverter : JsonConverter<Guid128>
+	{
+		public override Guid128 ReadJson(JsonReader reader, Type objectType, Guid128 existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			try
+			{
+				if (Guid128.TryParse((string)reader.Value, out Guid128 id))
+				{
+					return id;
+				}
+			}
+			catch { }
+
+			return Guid128.Empty;
+		}
+
+		public override void WriteJson(JsonWriter writer, Guid128 value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value.ToString());
+		}
+	}
+}
---
+++ runtime/Guid64JsonConverter.cs
@@ -0,0 +1,30 @@
+using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+
+namespace UniqueIdentifiers
+{
+	public class Guid64JsonConverter : JsonConverter<Guid64>
+	{
+		public override Guid64 ReadJson(JsonReader reader, Type objectType, Guid64 existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			try
+			{
+				if (Guid64.TryParse((string)reader.Value, out Guid64 id))
+				{
+					return id;
+				}
+			}
+			catch { }
+
+			return Guid64.Empty;
+		}
+
+		public override void WriteJson(JsonWriter writer, Guid64 value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value.ToString());
+		}
+	}
+}
